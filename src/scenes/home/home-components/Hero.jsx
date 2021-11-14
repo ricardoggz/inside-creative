244fe0cb6c765c5daeac1ddda474905d10da8f63
ricardoggz <ricardo_ggzicm@outlook.com>
2021-11-13 22:32:19 -0600

    home finished

--- src/scenes/home/home-components/Hero.jsx
+++ src/scenes/home/home-components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../home-styles/hero.css";
 
+import logohome from "../../../img/imgHome/logo.jpg";
+
 const Hero = () => {
   return(
     <section className="section-hero">
-      <div className="hero-container">
-        <span>Desarrollo de proyectos digitales a tu medida</span>
-        <p>Un giro a tu negocio como nunca...</p>
+      <div className="flex-container hero-container">
+        <img src={logohome} alt="loading"/>
+        <span>Un giro a tu negocio como nunca</span>
       </div>
     </section>
   )
